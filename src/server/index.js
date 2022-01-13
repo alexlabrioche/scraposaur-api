62b5d0545faa5a2730373f4c0fb4fd76ba98cdbe
Alex <alex@lepermislibre.fr>
2022-01-13 18:10:56 +0100

    minor fixes

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 import Hapi from "hapi";
-import Company from "./api/company";
-import NotFound from "./api/not-found";
+import Company from "./routes/company";
+import NotFound from "./routes/not-found";
 
 const createServer = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 7843,
     host: process.env.HOST || "localhost",
+    routes: { cors: { origin: ["*"] } },
   });
 
   await server.register([Company, NotFound]);
